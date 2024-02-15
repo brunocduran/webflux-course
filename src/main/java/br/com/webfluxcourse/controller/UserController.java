3e24e27ee3438fcdfa900b690be0fcef1b8c6cdb
brunocduran <brunocesarduran@gmail.com>
2024-02-14 23:50:19 -0300

    add requests and response structure tu user

--- src/main/java/br/com/webfluxcourse/controller/UserController.java
+++ src/main/java/br/com/webfluxcourse/controller/UserController.java
@@ -1,5 +1,7 @@
 package br.com.webfluxcourse.controller;
 
+import br.com.webfluxcourse.model.request.UserRequest;
+import br.com.webfluxcourse.model.response.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -11,13 +13,13 @@ public interface UserController {
     ResponseEntity<Mono<Void>> save(@RequestBody UserRequest request);
 
     @GetMapping(value = "/{id}")
-    ResponseEntity<Mono<UserRequest>> find(@PathVariable String id);
+    ResponseEntity<Mono<UserResponse>> find(@PathVariable String id);
 
     @GetMapping
-    ResponseEntity<Flux<UserRequest>> findAll();
+    ResponseEntity<Flux<UserResponse>> findAll();
 
     @PatchMapping(value = "/{id}")
-    ResponseEntity<Mono<UserRequest>> update(@PathVariable String id, @RequestBody UserRequest request);
+    ResponseEntity<Mono<UserResponse>> update(@PathVariable String id, @RequestBody UserRequest request);
 
     @DeleteMapping(value = "/{id}")
     ResponseEntity<Mono<Void>> delete(@PathVariable String id);
