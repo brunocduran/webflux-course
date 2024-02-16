d7a70fbc5c2fb155bc9fead231561960e2d7b5cc
brunocduran <brunocesarduran@gmail.com>
2024-02-15 23:33:24 -0300

    add mapper to user and impl to save contract user controller

---
+++ src/main/java/br/com/webfluxcourse/service/UserService.java
@@ -0,0 +1,21 @@
+package br.com.webfluxcourse.service;
+
+import br.com.webfluxcourse.entity.User;
+import br.com.webfluxcourse.mapper.UserMapper;
+import br.com.webfluxcourse.model.request.UserRequest;
+import br.com.webfluxcourse.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository repository;
+    private final UserMapper mapper;
+
+    public Mono<User> save(final UserRequest request){
+        return repository.save(mapper.toEntity(request));
+    }
+}
