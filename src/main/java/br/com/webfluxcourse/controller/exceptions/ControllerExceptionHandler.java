6215a24c7ec08c028c79732e44c30c5014efbcb4
brunocduran <brunocesarduran@gmail.com>
2024-02-18 21:59:30 -0300

    add exception handler to object not found

--- src/main/java/br/com/webfluxcourse/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/br/com/webfluxcourse/controller/exceptions/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.webfluxcourse.controller.exceptions;
 
+import br.com.webfluxcourse.service.exceprion.ObjectNotFoundException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 import static java.time.LocalDateTime.now;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -45,6 +47,23 @@ public class ControllerExceptionHandler {
         }
 
         return ResponseEntity.status(BAD_REQUEST).body(Mono.just(error));
+    }
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    ResponseEntity<Mono<StandardError>> objectNotFoundException(
+            ObjectNotFoundException ex, ServerHttpRequest request
+    ){
+        return ResponseEntity.status(NOT_FOUND)
+                .body(Mono.just(
+                        StandardError.builder()
+                                .timestamp(now())
+                                .status(NOT_FOUND.value())
+                                .error(NOT_FOUND.getReasonPhrase())
+                                .message(ex.getMessage())
+                                .path(request.getPath().toString())
+                                .build()
+                )
+        );
     }
 
     private String verifyDupKey(String message){
--- src/main/java/br/com/webfluxcourse/service/UserService.java
+++ src/main/java/br/com/webfluxcourse/service/UserService.java
@@ -4,9 +4,11 @@ import br.com.webfluxcourse.entity.User;
 import br.com.webfluxcourse.mapper.UserMapper;
 import br.com.webfluxcourse.model.request.UserRequest;
 import br.com.webfluxcourse.repository.UserRepository;
+import br.com.webfluxcourse.service.exceprion.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import static java.lang.String.format;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +22,11 @@ public class UserService {
     }
 
     public Mono<User> findById(final String id){
-        return repository.findById(id);
+        return repository.findById(id)
+                .switchIfEmpty(Mono.error(
+                        new ObjectNotFoundException(
+                                format("Object not found. Id: %s, type: %s ", id, User.class.getSimpleName())
+                        )
+                ));
     }
 }
