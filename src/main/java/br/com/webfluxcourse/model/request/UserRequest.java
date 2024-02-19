85f3cec23f36647509c9e5f0b98e5a2d31aec600
brunocduran <brunocesarduran@gmail.com>
2024-02-18 21:27:44 -0300

    add string validator

--- src/main/java/br/com/webfluxcourse/model/request/UserRequest.java
+++ src/main/java/br/com/webfluxcourse/model/request/UserRequest.java
@@ -1,19 +1,23 @@
 package br.com.webfluxcourse.model.request;
 
+import br.com.webfluxcourse.validator.TrimString;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequest(
 
+        @TrimString
         @Size(min = 3, max = 50, message = "Must be between 3 and 50 characters")
         @NotBlank(message = "Must not be null or empty")
         String name,
 
+        @TrimString
         @Email(message = "Invalid e-mail")
         @NotBlank(message = "Must not be null or empty")
         String email,
 
+        @TrimString
         @Size(min = 3, max = 20, message = "Must be between 3 and 20 characters")
         @NotBlank(message = "Must not be null or empty")
         String password
---
+++ src/main/java/br/com/webfluxcourse/validator/TrimStringValidator.java
@@ -0,0 +1,12 @@
+package br.com.webfluxcourse.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class TrimStringValidator implements ConstraintValidator<TrimString, String> {
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        return value == null || value.trim().length() == value.length();
+    }
+}
