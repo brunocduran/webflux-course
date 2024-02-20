151a57035ca5a7d850d785c954ab8d0c8c9e6802
brunocduran <brunocesarduran@gmail.com>
2024-02-19 21:14:24 -0300

    add unit test to update user service

--- src/test/java/br/com/webfluxcourse/service/UserServiceTest.java
+++ src/test/java/br/com/webfluxcourse/service/UserServiceTest.java
@@ -13,10 +13,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
-
-import java.util.Objects;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
@@ -78,5 +74,24 @@ class UserServiceTest {
                 .verify();
 
         Mockito.verify(repository, times(1)).findAll();
+    }
+
+    @Test
+    void testUpdate() {
+        UserRequest request = new UserRequest("Bruno", "bruno@gmail.com", "123");
+        User entity = User.builder().build();
+
+        when(mapper.toEntity(any(UserRequest.class), any(User.class))).thenReturn(entity);
+        when(repository.findById(anyString())).thenReturn(Mono.just(entity));
+        when(repository.save(any(User.class))).thenReturn(Mono.just(entity));
+
+        Mono<User> result = service.update("123", request);
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(repository, times(1)).save(any(User.class));
     }
 }
